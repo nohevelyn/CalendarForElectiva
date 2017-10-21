3e3d3eea65a57e0a80979426b9721b252600e6c8
KaozPain <32975344+KaozPain@users.noreply.github.com>
2017-10-21 13:49:07 -0500

    ok

--- app/src/main/java/com/example/adrian/calendar/MainActivity.java
+++ app/src/main/java/com/example/adrian/calendar/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.adrian.calendar;
 
+import android.icu.text.SimpleDateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boton=(Button)findViewById(R.id.Boton_Prueba);
+        boton.setText("Click en mi");
+    }
+
+    public void cambiarTexto(View v){
+
+        String s="Esta es una prueba";
+        TextView texto=(TextView) findViewById(R.id.TextQueDebeCambiar);
+        texto.setText(s);
     }
 }
