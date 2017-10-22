fee0c7b8373c8b38562026d18a26b249bccd969d
KaozPain <32975344+KaozPain@users.noreply.github.com>
2017-10-22 13:26:11 -0500

    ok

---
+++ app/src/main/java/com/example/adrian/calendar/Usuario.java
@@ -0,0 +1,57 @@
+package com.example.adrian.calendar;
+
+/**
+ * Created by adrian on 21/10/17.
+ */
+
+public class Usuario {
+
+
+    public Usuario() {}
+
+    public String nombre=null;
+    public String email=null;
+    public String clave=null;
+    public String cedula=null;
+    public String telefono=null;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
